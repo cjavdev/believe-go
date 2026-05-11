60be3096f439fdd01573f5946c88824141c8faac
CJ Avilla <avilla@stainless.com>
2026-05-11 19:40:49 -0400

    Build SDK
    Stainless-Generated-From: 8131c30b5025bea85b85e40b0c2c6bf545d74ab2

---
+++ usage_test.go
@@ -0,0 +1,33 @@
+// File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+package believe_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/cjavdev/believe-go"
+	"github.com/cjavdev/believe-go/internal/testutil"
+	"github.com/cjavdev/believe-go/option"
+)
+
+func TestUsage(t *testing.T) {
+	t.Skip("Mock server tests are disabled")
+	baseURL := "http://localhost:4010"
+	if envURL, ok := os.LookupEnv("TEST_API_BASE_URL"); ok {
+		baseURL = envURL
+	}
+	if !testutil.CheckTestServer(t, baseURL) {
+		return
+	}
+	client := believe.NewClient(
+		option.WithBaseURL(baseURL),
+		option.WithAPIKey("My API Key"),
+	)
+	page, err := client.Characters.List(context.TODO(), believe.CharacterListParams{})
+	if err != nil {
+		t.Fatalf("err should be nil: %s", err.Error())
+	}
+	t.Logf("%+v\n", page)
+}
